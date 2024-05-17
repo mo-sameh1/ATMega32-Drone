e3060574a622d40ad000d4782522ab93f0a9283e
sir3bdo <112068649+sir3bdo@users.noreply.github.com>
2024-05-17 11:29:04 +0300

    NEW

---
+++ GccApplication2/GccApplication2/FLIGHT_INIT.h
@@ -0,0 +1,16 @@
+/*
+ * FLIGHT_INIT.h
+ *
+ * Created: 5/17/2024 11:28:27 AM
+ *  Author: elaqsa
+ */ 
+
+
+#ifndef FLIGHT_INIT_H_
+#define FLIGHT_INIT_H_
+
+
+
+
+
+#endif /* FLIGHT_INIT_H_ */
--- GccApplication2/GccApplication2/main.c
+++ GccApplication2/GccApplication2/main.c
@@ -5,7 +5,7 @@
 #include "BIT_math.h"
 #include "PWM_int.h"
 #include "DIO_int.h"
-#include "uart.h"
+#include "USART_H.h"
 int main(void)
 {
 	DDRA|=0x0F;
@@ -18,15 +18,14 @@ int main(void)
 	DIO_voidSetPinDir(DIO_U8_PORTD, DIO_U8_PIN7, DIO_U8_OUTPUT);
 	DIO_voidSetPinValue(DIO_U8_PORTD, DIO_U8_PIN2, DIO_U8_LOW);
 	DIO_voidSetPinValue(DIO_U8_PORTD, DIO_U8_PIN3, DIO_U8_HIGH);
-	
     PWM_vidInit();
-	UART_init();
+	USART_Init(9600);
     while (1) 
     {
 		
-		u8 rec=UART_recieveByte();
-		f32 rec_send=rec;
-		PWM_vidSetDutyNFreqCH0(rec_send);				
+		u8 rec=USART_RxChar();
+		
+						
     }
 }
 
